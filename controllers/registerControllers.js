fd4783f96f6832b5a8668f45be569193f8470164
manuelsrl <manuelschallerrosal@gmail.com>
2024-08-23 14:45:57 -0300

    signupPost function working correctly

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -8,12 +8,20 @@ export const signupGet = (req, res) => {
 // Function to handle POST request for signup
 export const signupPost = async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({ message: "Email & password are required." });
+
+  // Duplicate on DB
+  const duplicate = User.findOne({ email }) || null;
+  if (duplicate !== null) {
+    return res.status(409).json({ message: "Email already registered" });
+  }
 
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
     console.log(err);
-    res.status(400).send("User not created");
+    res.status(500).send("User not created");
   }
 };
