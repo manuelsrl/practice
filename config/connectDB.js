7cad8c3c1f281182fab11ca9cfb32e1659fae85d
manuelsrl <manuelschallerrosal@gmail.com>
2024-08-23 13:38:39 -0300

    DB URI moved to env variables

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    const connection = await mongoose.connect(process.env.DB_URI);
     connection.STATES.connected
       ? console.log("MongoDB connected")
       : console.log("Error connecting MongoDB");
