a5fc2b704c5b0a067a6f1e60000d9dbc625d9d42
manuelsrl <manuelschallerrosal@gmail.com>
2024-08-24 12:30:59 -0300

    Morgan middleware and promise to connect DB added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import corsOptions from "./config/corsOptions.js";
 import { router as authRouter } from "./routes/authRoutes.js";
 import connectDB from "./config/connectDB.js";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -10,12 +11,14 @@ const app = express();
 // Middlewares
 //  - CORS
 app.use(cors(corsOptions));
-
 //  - Public folder
 app.use(express.static("public"));
-
 //  - JSON middleware
 app.use(express.json());
+//  - Morgan
+app.use(morgan("tiny"));
+
+app.disable("x-powered-by");
 
 // Home page
 app.get("/", (req, res) => {
@@ -25,8 +28,12 @@ app.get("/", (req, res) => {
 // authRouter => /signup GET & POST, /login GET & POST /logout GET
 app.use("/auth", authRouter);
 
-app.listen(PORT, () => {
-  // Connection to MongoDB
-  connectDB();
-  console.log(`Server listening on port ${PORT}...`);
+connectDB().then(() => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}...`);
+    });
+  } catch (err) {
+    console.log("Cannot connect to the server");
+  }
 });
